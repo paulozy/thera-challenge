70fa5f26ffefd3496e4329a2ae4319f35c1d2c38
paulozy <dev.pa.aabreu18@gmail.com>
2025-07-05 20:54:28 -0300

    feat: remove app controller and add health check module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './infra/database/database.module';
 import { GatewayModule } from './infra/gateways/gateway.module';
 import { LoggingMiddleware } from './infra/http/middlewares/logging.middleware';
 import { AuthModule } from './modules/auth/auth.module';
+import { HealthModule } from './modules/health/health.module';
 import { OrdersModule } from './modules/orders/orders.module';
 import { ProductsModule } from './modules/products/products.module';
 import { UsersModule } from './modules/users/users.module';
@@ -17,9 +16,8 @@ import { UsersModule } from './modules/users/users.module';
     UsersModule,
     OrdersModule,
     AuthModule,
+    HealthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './infra/http/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Thera-Chall API')
+    .setDescription('The Thera-Chall API description')
+    .setVersion('1.0')
+    .addTag('thera-chall')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
